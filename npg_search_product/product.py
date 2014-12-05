41b30f06a40f0fc05b2032c41861c0b443af2f73
stephen <stephen@stephen-Latitude-D820>
2014-12-05 00:48:38 -0500

    Add module npg_search_product allow search on supplier in purchase order

---
+++ npg_search_product/product.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 NovaPoint Group LLC (<http://www.novapointgroup.com>)
+#    Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import time
+from openerp import pooler
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, DATETIME_FORMATS_MAP, float_compare
+import openerp.addons.decimal_precision as dp
+from openerp import netsvc
+
+class product_product(osv.osv):
+    _inherit = 'product.product'
+
+    def search(self, cr, uid, args, offset=0, limit=None, order=None,
+            context=None, count=False):
+        if context.get('partner_id', False):
+            prod_supp_info = self.pool.get('product.supplierinfo')
+            prod_supp_ids = prod_supp_info.search(cr, uid, [('name', '=', context['partner_id'])], context=context)
+            IDS = [x.product_id.id for x in prod_supp_info.browse(cr, uid, prod_supp_ids, context=context)]
+            args += [('id', 'in', IDS)]
+
+        return super(product_product, self).search(cr, uid, args, offset, limit,
+                order, context=context, count=count)
+
+product_product()
+
