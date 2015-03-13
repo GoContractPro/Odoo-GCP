73d253b88c8b3ba827caae4d0477d5007892ea80
Ruby Chauhan <ruby.chauhan@verts.co.in>
2015-03-13 19:24:17 +0530

    Added attribute on delivery order form and get_rate functionality

--- shipping_api_ups/stock.py
+++ shipping_api_ups/stock.py
@@ -183,6 +183,36 @@ class stock_picking(osv.osv):
             res['value']['shipper'] = ups_shipper_id  
 
         return res
+    
+    def onchage_service(self, cr, uid, ids, ups_shipper_id=False, context=None):
+         vals = {}
+         service_type_ids = []
+         if ups_shipper_id:
+             shipper_obj = self.pool.get('ups.account.shipping').browse(cr, uid, ups_shipper_id)
+             for shipper in shipper_obj.ups_shipping_service_ids:
+                 service_type_ids.append(shipper.id)
+         domain = [('id', 'in', service_type_ids)]
+         return {'domain': {'ups_service_id': domain}}
+    
+    def onchange_ups_shipper_id(self, cr, uid, ids, ups_shipper_id = False, context=None):
+
+        res = {}
+        
+        service_type_ids = []
+        if ups_shipper_id:
+            shipper_obj = self.pool.get('ups.account.shipping').browse(cr, uid, ups_shipper_id)
+            for shipper in shipper_obj.ups_shipping_service_ids:
+                service_type_ids.append(shipper.id)
+        domain = [('id', 'in', service_type_ids)]
+        
+        if ups_shipper_id:
+            partner_id = self.pool.get('ups.account.shipping').browse(cr, uid, ups_shipper_id, context=context).partner_id.id
+            res = {'value': {'transport_id' : partner_id},
+                   'domain': {'ups_service_id': domain}}
+        return res
+    
+    
+    
 
 #     def action_process(self, cr, uid, ids, context=None):
 #         sale_order_line = []
@@ -1302,6 +1332,192 @@ class stock_picking(osv.osv):
                            })
                         inv_obj.button_reset_taxes(cr, uid, result.values(), context=context)
         return result_partial
+    
+    
+    def get_rate(self, cr, uid, ids, context=None):
+        
+        pick_obj = self.pool.get('sale.order')
+        
+        data = self.browse(cr, uid, ids[0], context=context)
+#        sale_obj.write(cr,uid,context.get('active_ids'),{'ups_shipper_id':data.ups_shipper_id.id,
+#                                                         'ups_service_id':data.ups_service_id.id,
+#                                                         'ups_pickup_type':data.ups_pickup_type,
+#                                                         'ups_packaging_type':data.ups_packaging_type.id},context=context)
+        if context is None:
+            context = {}
+#        if not (data['rate_selection'] == 'rate_request' and data['ship_company_code'] == 'ups'):
+#            return super(shipping_rate_wizard, self).get_rate(cr, uid, ids, context)
+#        if context.get('active_model', False) == 'sale.order':
+        weight = data.tot_order_weight or 0.00
+        
+#         invoice = self.pool.get('account.invoice').browse(cr, uid, context['active_id'], context=context)
+#         weight = invoice.total_weight_net or 0.00
+        receipient_zip = data.partner_id and data.partner_id.zip or ''
+        receipient_country_code = data.partner_id.country_id and data.partner_id.country_id.code or ''
+        access_license_no = data.shipper and  data.shipper.accesslicensenumber or ''
+        user_id = data.shipper and  data.shipper.userid or ''
+        password = data.shipper and data.shipper.password or ''
+        pickup_type_ups = data.ups_pickup_type
+        shipper_zip = data.shipper and data.shipper.address and data.shipper.address.zip or ''
+        shipper_country_code =  data.shipper and data.shipper.address and  data.shipper.address.country_id and \
+                                data.shipper.address.country_id.code or ''
+        ups_info_shipper_no = data.shipper and data.shipper.acc_no or ''
+        
+        packaging_type_ups = data.ups_packaging_type.code
+        test_mode = False
+        test_mode = data.delivery_method and data.delivery_method.test_mode
+        
+        if test_mode:
+            url = unicode(data.delivery_method.ship_rate_test_web)
+        #                port = data.logis_company.ship_rate_test_port
+        else:
+            url = unicode(data.delivery_method.ship_rate_web)
+#                port = data.logis_company.ship_rate_port
+    
+#         if data.ups_service_id:
+#            request_action ="rate"
+#            request_option ="rate"
+#            service_type_ups = data.ups_service_id and data.ups_service_id.shipping_service_code or ''            
+#         else:
+        request_action = "shop"
+        request_option = "shop"
+        service_type_ups = ''
+
+#            url = 'https://wwwcie.ups.com/ups.app/xml/Rate' or 'https://onlinetools.ups.com/ups.app/xml/Rate'
+
+        rate_request = """<?xml version=\"1.0\"?>
+         <AccessRequest xml:lang=\"en-US\">
+             <AccessLicenseNumber>%s</AccessLicenseNumber>
+             <UserId>%s</UserId>
+             <Password>%s</Password>
+         </AccessRequest>
+         <?xml version=\"1.0\"?>
+         <RatingServiceSelectionRequest xml:lang=\"en-US\">
+             <Request>
+                 <TransactionReference>
+                     <CustomerContext>Rating and Service</CustomerContext>
+                     <XpciVersion>1.0001</XpciVersion>
+                 </TransactionReference>
+                 <RequestAction>%s</RequestAction>
+                 <RequestOption>%s</RequestOption>
+             </Request>
+         <PickupType>
+             <Code>%s</Code>
+         </PickupType>
+         <Shipment>
+             <Shipper>
+                 <Address>
+                     <PostalCode>%s</PostalCode>
+                     <CountryCode>%s</CountryCode>
+                 </Address>
+             <ShipperNumber>%s</ShipperNumber>
+             </Shipper>
+             <ShipTo>
+                 <Address>
+                     <PostalCode>%s</PostalCode>
+                     <CountryCode>%s</CountryCode>
+                 <ResidentialAddressIndicator/>
+                 </Address>
+             </ShipTo>
+             <ShipFrom>
+                 <Address>
+                     <PostalCode>%s</PostalCode>
+                     <CountryCode>%s</CountryCode>
+                 </Address>
+             </ShipFrom>
+             <Service>
+                 <Code>%s</Code>
+             </Service>
+             <Package>
+                 <PackagingType>
+                     <Code>%s</Code>
+                 </PackagingType>
+                 <PackageWeight>
+                     <UnitOfMeasurement>
+                         <Code>LBS</Code>
+                     </UnitOfMeasurement>
+                     <Weight>%s</Weight>
+                 </PackageWeight>
+             </Package>
+         </Shipment>
+         </RatingServiceSelectionRequest>""" % (access_license_no, user_id, password,request_action,request_option, pickup_type_ups, shipper_zip,shipper_country_code, 
+                                                ups_info_shipper_no,receipient_zip, receipient_country_code, shipper_zip, shipper_country_code, 
+                                                service_type_ups, packaging_type_ups, weight)
+         
+        rates_obj = self.pool.get('shipping.rates.sales')
+        so = data.id
+        rids = rates_obj.search(cr,uid,[('sales_id','=', so )])
+        rates_obj.unlink(cr, uid, rids, context)
+        serv_obj = self.pool.get('ups.shipping.service.type')
+    
+        try:
+            print rate_request
+            from urllib2 import Request, urlopen, URLError, quote
+            request = Request(url.encode('utf-8').strip(), rate_request.encode('utf-8').strip())
+            response_text = urlopen(request).read()
+        #             p.agent_info = u' '.join((agent_contact, agent_telno)).encode('utf-8').strip()
+            print response_text
+            
+            response_dic = xml2dic.main(response_text)
+            str_error = ''
+            for response in response_dic['RatingServiceSelectionResponse'][0]['Response']:
+                if response.get('Error'):
+                    for item in response['Error']:
+                        if item.get('ErrorDescription'):
+                            str_error = item['ErrorDescription']
+                            self.write(cr, uid, [data.id], {'status_message': "Error : " + item['ErrorDescription'] })
+            if not str_error:
+        #                     print response_dic
+        
+                amount = None
+                ups_service_id = None
+        # Get all the return Shipping rates as options                    
+                for response in response_dic['RatingServiceSelectionResponse']:
+                    
+                    
+                    if response.get('RatedShipment'):
+                        
+                        warning = None
+                        vals = {}
+                        for val in response['RatedShipment']:
+                            if val.get('TotalCharges'):
+                                vals['totalcharges'] = float(val['TotalCharges'][1]['MonetaryValue'])
+                            if val.get('GuaranteedDaysToDelivery'):
+                                vals['daystodelivery'] = val['GuaranteedDaysToDelivery']
+                            if val.get('Service'):
+                                service_code = val['Service'][0]['Code'] 
+                                service = serv_obj.search(cr,uid,[('shipping_service_code','=',service_code)])                 
+                                vals['service'] =service[0]
+                            if val.get('RatedShipmentWarning'):
+                                if not warning:
+                                    warning =  val['RatedShipmentWarning']
+                                else:
+                                    warning = warning + ", " + val['RatedShipmentWarning']
+                                     
+                        # get the lowest cost shipping rate as default on Sales Order                                       
+                                     
+                        if (amount is None) or amount > vals['totalcharges']: 
+                            amount = vals['totalcharges']
+                            ups_service_id = vals['service']
+                            status_mesage = warning 
+                            
+                        vals['ratedshipmentwarning'] = warning
+                        vals['sales_id'] = so
+                        rates_obj.create(cr,uid,vals,context)
+                pick_obj.write(cr,uid,so,{'shipcharge':amount or 0.00,'ups_service_id':ups_service_id,'status_message':warning},context=context)
+        
+                return True
+                rates_obj.write(cr, uid, context.get('active_ids'), { 'status_message': 'Success!'},context=context)
+        except URLError, e:
+            if hasattr(e, 'reason'):
+                print 'Could not reach the server, reason: %s' % e.reason
+            elif hasattr(e, 'code'):
+                print 'Could not fulfill the request, code: %d' % e.code
+            raise
+           
+       
+    #         mod, modid = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'shipping_api_ups', 'view_for_shipping_rate_wizard_shipping')
+        return True
 
 stock_picking()
 
