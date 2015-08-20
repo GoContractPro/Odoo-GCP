ed7d4a8d6f1c6b9736670024525e716c466d36fa
Stephen Levenhagen <slevenhagen@novapointgroup.com>
2015-08-19 22:31:32 -0400

    Update Print Button on Sales for new Report

---
+++ custom_sale_order_report/sale.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 NovaPoint Group INC (<http://www.novapointgroup.com>)
+#    Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+from openerp import models, fields, api, exceptions, _
+
+class sale_order(models.Model):
+    _inherit = "sale.order"
+
+    @api.multi
+    def print_quotation(self):
+        '''
+        This function prints the sales order and mark it as sent, so that we can see more easily the next step of the workflow
+        '''
+        assert len(self.ids) == 1, 'This option should only be used for a single id at a time'
+        self.signal_workflow( 'quotation_sent')
+        return self.env['report'].get_action(self,'custom_sale_order_report.custom_report_saleorder')
