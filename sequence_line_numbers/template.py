2701c64f1cf232855ccba45742e87cb056c88041
Stephen Levenhagen <slevenhagen@novapointgroup.com>
2015-09-23 12:57:33 -0400

    mid point commit

---
+++ sequence_line_numbers/template.py
@@ -0,0 +1,60 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+import math
+
+from openerp.addons.product import _common
+from openerp import tools
+
+class SequenceLineIncrement(models.Model):
+    name= 'sequenceLineIncrement'
+    
+    model = fields.Many2one()
+    parent_model = fields.Many2one()
+    parent_id = fields.Integer()
+    max_sequence = fields.integer()
+    
+    
+    #TODO Rewrite this method for new Line Sequence models
+    @api.multi
+    def _next(self, number_increment,parent_model,model):
+        if not ids:
+            return False
+        if context is None:
+            context = {}
+        force_company = context.get('force_company')
+        if not force_company:
+            force_company = self.pool.get('res.users').browse(cr, uid, uid).company_id.id
+        sequences = self.read(cr, uid, ids, ['name','company_id','implementation','number_next','prefix','suffix','padding'])
+        preferred_sequences = [s for s in sequences if s['company_id'] and s['company_id'][0] == force_company ]
+        seq = preferred_sequences[0] if preferred_sequences else sequences[0]
+        if seq['implementation'] == 'standard':
+            cr.execute("SELECT nextval('ir_sequence_%03d')" % seq['id'])
+            seq['number_next'] = cr.fetchone()
+        else:
+            cr.execute("SELECT number_next FROM ir_sequence WHERE id=%s FOR UPDATE NOWAIT", (seq['id'],))
+            cr.execute("UPDATE ir_sequence SET number_next=number_next+number_increment WHERE id=%s ", (seq['id'],))
+            self.invalidate_cache(cr, uid, ['number_next'], [seq['id']], context=context)
+        d = self._interpolation_dict()
+        try:
+            interpolated_prefix = self._interpolate(seq['prefix'], d)
+            interpolated_suffix = self._interpolate(seq['suffix'], d)
+        except ValueError:
+            raise osv.except_osv(_('Warning'), _('Invalid prefix or suffix for sequence \'%s\'') % (seq.get('name')))
+        return interpolated_prefix + '%%0%sd' % seq['padding'] % seq['number_next'] + interpolated_suffix
---
+++ shipping_api/delivery_method.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 NovaPoint Group LLC (<http://www.novapointgroup.com>)
+#    Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+
+
+class delivery_method(osv.osv):
+    _inherit = "delivery.method"
+    
+    
+    def _get_company_code(self, cr, user, context=None):
+        res =  super(delivery_method, self)._get_company_code(cr, user, context=context)
+        res.append(('api', 'API'))
+        return res
