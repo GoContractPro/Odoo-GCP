5c9ccb304e4b1c56ac64f570a1437d35986c0131
Stephen Levenhagen <slevenhagen@novapointgroup.com>
2015-10-06 15:52:10 -0400

    Modification to NPG Import for DBF File Format

--- npg_import/__init__.py
+++ npg_import/__init__.py
@@ -19,6 +19,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 ##############################################################################
-import import_data
+from . import models
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
