184090ca1aa657d9126818e448f20d7ad7a20f97
Stephen Levenhagen <slevenhagen@novapointgroup.com>
2016-02-02 10:41:21 -0500

    UPdate import data search on import source fields

--- npg_import/models/import_data9_0.py
+++ npg_import/models/import_data9_0.py
@@ -501,9 +501,8 @@ class import_data_file(models.Model):
                 return False
   
             elif res_id == 0: 
-                res_id = self.create_import_record(vals=vals, external_id_name=external_id_name,model=model)
-                return res_id
-                
+                 res_id = self.create_import_record(vals=vals, external_id_name=external_id_name, model=model)
+                 return res_id
                 
             elif res_id and self.do_update:
                 record_obj = self.env[model].browse(res_id)
